14414984ddbf7734e294dcbf97d07659c4a4e17b
bewpage <b.jedlinski@bewpage.com>
2017-05-23 14:01:38 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let formData = document.getElementById('myForm');
+    let email = document.getElementById("email");
+    let name = document.getElementById("name");
+
+
+    formData.addEventListener('submit', function(e){
+    e.preventDefault();
+    let formValidation = true;
+    let allErrors = [];
+
+
+
+    formData.submit();
+    console.log(formValue.value);
+    // console.log();
+})
+
+
+});
