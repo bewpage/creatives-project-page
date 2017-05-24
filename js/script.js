b74d267d1e035b905b9f6c16a3b064b998c06f85
bewpage <b.jedlinski@bewpage.com>
2017-05-24 14:47:05 +0200

    fixed menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let formData = document.getElementById('myForm');
-    let email = document.getElementById("email");
-    let name = document.getElementById("name");
+//     let formData = document.getElementById('myForm');
+//     let email = document.getElementById("email");
+//     let name = document.getElementById("name");
+//
+//
+//     formData.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     let formValidation = true;
+//     let allErrors = [];
+//
+//
+//
+//     formData.submit();
+//     console.log(formValue.value);
+//     // console.log();
+// })
 
 
-    formData.addEventListener('submit', function(e){
-    e.preventDefault();
-    let formValidation = true;
-    let allErrors = [];
+// jQuery("document").ready(function($){
 
 
 
-    formData.submit();
-    console.log(formValue.value);
-    // console.log();
-})
+        let nav = $('.header__main-nav');
+        let bgNav = $('.header__menu-fix');
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                nav.addClass('f-nav-fix');
+                bgNav.addClass('b-nav-fix');
+            } else {
+                nav.removeClass('f-nav-fix');
+                bgNav.removeClass('b-nav-fix');
+            }
+        });
+
+    // });
+
 
 
 });
