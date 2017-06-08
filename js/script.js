480e28ca9900a526f7853617670ec12d618d3746
bewpage <b.jedlinski@bewpage.com>
2017-06-08 16:56:44 +0200

    update

--- js/script.js
+++ js/script.js
@@ -17,24 +17,34 @@ document.addEventListener("DOMContentLoaded", function() {
 // })
 
 
-// jQuery("document").ready(function($){
 
 
 
         let nav = $('.header__main-nav');
+        let mobileNav = $('.toggle-nav');
         let bgNav = $('.header__menu-fix');
+        let dropDownMenu = $('.header__main-nav__menu');
 
         $(window).scroll(function () {
             if ($(this).scrollTop() > 50) {
                 nav.addClass('f-nav-fix');
+                mobileNav.addClass('f-nav-fix');
                 bgNav.addClass('b-nav-fix');
             } else {
                 nav.removeClass('f-nav-fix');
+                mobileNav.removeClass('f-nav-fix');
                 bgNav.removeClass('b-nav-fix');
             }
         });
 
-    // });
+    mobileNav.click(function(e) {
+        e.preventDefault();
+        // $(this).toggleClass('active');
+        dropDownMenu.toggleClass('active');
+
+    });
+
+
 
 
 
